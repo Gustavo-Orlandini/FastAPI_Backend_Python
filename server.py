51faa103726888182f236bd0b2e1cb6c4c8638eb
*Gustavo <guga77sugu@hotmail.com>
2023-08-08 12:04:24 -0300

    Route params

--- server.py
+++ server.py
@@ -2,22 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"mensagem": "Olá fastAPI - TDS"}
+@app.get("/saudacao/{nome}")
+async def home(nome):
+    return nome
 
-@app.get("/profile")
-async def profile():
-    return {"name": "Gustavo Suguyama Orlanidni"}
-
-@app.post("/profile")
-async def signup():
-    return {"mensagem": "Perfil criado com sucesso"}
-
-@app.put("/profile")
-async def atualizar():
-    return {"mensagem": "Perfil atualizado com sucesso"}
-
-@app.delete("/profile")
-async def remover():
-    return {"mensagem": "Perfil deletado com sucesso"}
