85cb96d1234bc61f48156620aa051fb73283bb9c
*Gustavo <guga77sugu@hotmail.com>
2023-08-08 15:57:10 -0300

    Get animal by ID

--- server.py
+++ server.py
@@ -17,10 +17,17 @@ banco: List[Animal] = []
 async def listar_animais():
     return banco
 
+@app.get('/animais/{animal_id}')
+def obter_animal(animal_id: str):
+    for animal in banco:
+        if animal.id == animal_id:
+            return animal
+    return {"error": "Nenhum animal encontrado"}    
+
 @app.post('/animais')
 def criar_animal(animal: Animal):
     if animal.id is None:
-        animal.id = uuid4()
+        animal.id = str(uuid4())
         banco.append(animal)
     return {"mensagem": f"{animal.nome} cadastrado com sucesso"}
 
