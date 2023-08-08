197f150c2085ab180126e04a37b174b85e009551
*Gustavo <guga77sugu@hotmail.com>
2023-08-08 11:54:56 -0300

    Create all endpoints

--- server.py
+++ server.py
@@ -4,8 +4,20 @@ app = FastAPI()
 
 @app.get("/")
 async def home():
-    return {"mensagem": "Olá fastAPI - TDS assasaasassas"}
+    return {"mensagem": "Olá fastAPI - TDS"}
 
 @app.get("/profile")
 async def profile():
     return {"name": "Gustavo Suguyama Orlanidni"}
+
+@app.post("/profile")
+async def signup():
+    return {"mensagem": "Perfil criado com sucesso"}
+
+@app.put("/profile")
+async def atualizar():
+    return {"mensagem": "Perfil atualizado com sucesso"}
+
+@app.delete("/profile")
+async def remover():
+    return {"mensagem": "Perfil deletado com sucesso"}
