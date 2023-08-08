3422b7a17c13000fc9be3d76d23530dc246df833
*Gustavo <guga77sugu@hotmail.com>
2023-08-08 14:08:26 -0300

    Create a Product and test in Insominia

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/saudacao/{nome}")
-async def home(nome):
-    return nome
+async def home(nome: str):
+    texto = f'Olá {nome}, seja bem vindo!'
+    return {"mensagem": texto}
 
+@app.get("/quadrado/{numero}")
+async def home(numero: int):
+    resultado = numero * numero
+    texto = f'O quadrado de {numero}, é {resultado}!'
+    return {"mensagem": texto}
+
+class Produto(BaseModel):
+    nome: str
+    preco: float
+
+@app.post('/produtos')
+def produtos(produto: Produto):
+    return {"mensagem": f'Produto ({produto}) cadastrado com sucesso!'}
