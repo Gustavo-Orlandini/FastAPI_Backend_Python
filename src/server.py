12d3d4bc958d5d390c9077660c31f0c391a5edab
*Gustavo <guga77sugu@hotmail.com>
2023-08-09 17:38:34 -0300

    Render first child on frontend

--- src/server.py
+++ src/server.py
@@ -1,9 +1,19 @@
 from uuid import uuid4
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 class Animal(BaseModel):
     id: int = None
     nome: str
