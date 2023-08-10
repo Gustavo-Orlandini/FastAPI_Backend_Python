f4e87faf4b46111b65e50f466f45322418faea26
*Gustavo <guga77sugu@hotmail.com>
2023-08-09 22:32:50 -0300

    Register new animal

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ async function carregarAnimais() {
     const response = await axios.get('http://localhost:8000/animais')
     const animais = response.data
     const lista = document.getElementById('lista-animais')
+    lista.innerHTML = ''
 
     animais.map(animal => {
         const item = document.createElement('li')
@@ -9,12 +10,33 @@ async function carregarAnimais() {
 
         lista.appendChild(item)
     })
+}
+
+async function manipularFormulário() {
+    const form_animal = document.getElementById('form-animal')
+    const input_nome = document.getElementById('nome')
+
+    form_animal.onsubmit = async (event) => {
+        event.preventDefault()
+        const nome_animal = input_nome.value
+
+        axios.post('http://localhost:8000/animais', {
+            nome: nome_animal,
+            idade: 2,
+            sexo: "Femea",
+            cor: "Marrom"
+        })
+        carregarAnimais()
+        alert('Animal cadastrado com sucesso!')
+    }
 
 }
+
 
 function app() {
     console.log('App iniciada')
     carregarAnimais()
+    manipularFormulário()
 }
 
 app()
